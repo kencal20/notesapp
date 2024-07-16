643db9cc113722eff36823c2b993d567484e2a69
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-07-16 13:11:29 +0000

    add setNotes prop , add handleDelete function and pass to NoteItem

--- src/components/noteList.tsx
+++ src/components/noteList.tsx
@@ -3,14 +3,19 @@ import { IState, Note } from '../objects'
 import NoteItem from './noteItem'
 type Props = {
     notes: IState['note'][]
+    setNotes: React.Dispatch<React.SetStateAction<Note[]>>
 }
 
-export default function NoteList({ notes }: Props) {
+export default function NoteList({ notes, setNotes }: Props) {
+
+    function handleDelete(id:string) {
+        setNotes(notes => notes.filter(note => note.id !== id))
+    }
     return (
         <div>
             {
                 notes.map(note =>
-                    (<NoteItem  key={note.id} note={note}/> ))
+                    (<NoteItem key={note.id} note={note} handleDelete={handleDelete} />))
             }
         </div>
     )
