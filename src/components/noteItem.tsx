cc1a678c7fe1bf206beda3920b2fb84e1059e1e7
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-07-16 13:13:20 +0000

    use of handleDelete prop from noteList, and pass it as an onClick the Button

--- src/components/noteItem.tsx
+++ src/components/noteItem.tsx
@@ -1,24 +1,21 @@
 import React from 'react'
 import { IState } from '../objects'
-import { Card } from 'react-bootstrap'
+import { Button, Card } from 'react-bootstrap'
 
 type Props = {
-    note:IState['note'],
+  note: IState['note']
+  handleDelete: (id: string) => void
 }
 
-export default function NoteItem({}: Props) {
+export default function NoteItem({ note,handleDelete }: Props) {
   return (
-    <Card style={{ width: '18rem' }}>
-    <Card.Body>
-      <Card.Title>Card Title</Card.Title>
-      <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-      <Card.Text>
-        Some quick example text to build on the card title and make up the
-        bulk of the card's content.
-      </Card.Text>
-      <Card.Link href="#">Card Link</Card.Link>
-      <Card.Link href="#">Another Link</Card.Link>
-    </Card.Body>
-  </Card>
+    <Card style={{ backgroundColor: note.color }}>
+      <Card.Body>
+        <Card.Title>{note.title}</Card.Title>
+        <Card.Text>{note.text}</Card.Text>
+        <Card.Subtitle className="mb-2 text-muted">{note.date}</Card.Subtitle>
+        <Button variant='danger' onClick={()=> handleDelete(note.id)}>Delete</Button>
+      </Card.Body>
+    </Card>
   )
 }
