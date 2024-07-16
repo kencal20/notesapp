6923aeb373315df48d988920c8eb65bb96d69683
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-07-16 12:46:04 +0000

    rendering noteList in app js

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React, { useState } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { IState } from './objects'
+import { Container, Row,Col } from 'react-bootstrap'
+import NoteList from './components/noteList'
 
 
 export default function App() {
-  const [notes,setNotes] = useState<IState['note'][]>([{
-    id:new Date().toDateString(),
+  const [notes, setNotes] = useState<IState['note'][]>([{
+    id: new Date().toDateString(),
     title: 'notes',
     text: 'notes',
     color: '#F5F5F5',
@@ -15,7 +17,13 @@ export default function App() {
   }])
   return (
     <div className='App'>
-      App
+      <Container>
+        <Row>
+          <Col>
+      <NoteList  notes={notes} />
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
