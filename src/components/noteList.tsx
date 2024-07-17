116963318a3c0863703801839ee83c987ef1f4e3
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-07-17 10:31:05 +0000

    removal of setNOtes prop ,handle delete function and calling handledelete prop

--- src/components/noteList.tsx
+++ src/components/noteList.tsx
@@ -1,22 +1,21 @@
 import React from 'react'
 import { IState, Note } from '../objects'
 import NoteItem from './noteItem'
+
 type Props = {
     notes: IState['note'][]
-    setNotes: React.Dispatch<React.SetStateAction<Note[]>>
+    handleDelete: (_id: string) => void
 }
 
-export default function NoteList({ notes, setNotes }: Props) {
+export default function NoteList({ notes,handleDelete }: Props) {
 
-    function handleDelete(id:string) {
-        setNotes(notes => notes.filter(note => note.id !== id))
-    }
+
     return (
         <div>
             <h2>Note List</h2>
             {
                 notes.map(note =>
-                    (<NoteItem key={note.id} note={note} handleDelete={handleDelete} />))
+                    (<NoteItem  key={note._id} note={note} handleDelete={handleDelete} />))
             }
         </div>
     )
