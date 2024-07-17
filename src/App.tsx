b484ba4be04cb26fb0e9e76c999fd88720e073c6
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-07-17 10:26:56 +0000

    making the note state an empty array on initialization calling get and delete functions from backend and passing them as props to the notelist component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,43 @@
-import React, { useState } from 'react'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { IState } from './objects'
-import { Container, Row, Col } from 'react-bootstrap'
-import NoteList from './components/noteList'
-import CreateNote from './components/createNote'
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { IState } from './objects';
+import { Container, Row, Col } from 'react-bootstrap';
+import NoteList from './components/noteList';
+import CreateNote from './components/createNote';
+import axios from 'axios';
 
 
 export default function App() {
-  const [notes, setNotes] = useState<IState['note'][]>([{
-    id: new Date().toDateString(),
-    title: 'notes',
-    text: 'notes',
-    color: '#F5F5F5',
-    date: new Date().toDateString()
+  const [notes, setNotes] = useState<IState['note'][]>([]);
 
-  }])
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  function fetchNotes() {
+    axios.get('http://localhost:5000/notes')
+      .then(res => {
+        console.log(res.data.notes);
+        setNotes(res.data.notes)
+      })
+  }
+  function deleteNote(id: string) {
+    axios.delete(`http://localhost:5000/note/${id}`)
+      .then(() => {
+        setNotes(notes => notes.filter(note => note._id !== id));
+      })
+      .catch((error) => {
+        console.error('There was an error deleting the note!', error);
+      });
+  }
+
   return (
-    <div >
+    <div>
       <Container className='mt-3'>
         <Row>
           <Col>
-            <NoteList notes={notes} setNotes={setNotes} />
+            <NoteList notes={notes} handleDelete={deleteNote} />
           </Col>
           <Col>
             <CreateNote notes={notes} setNotes={setNotes} />
@@ -29,5 +45,5 @@ export default function App() {
         </Row>
       </Container>
     </div>
-  )
+  );
 }
